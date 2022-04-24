0dfd9d928b8669817f216a5d59a3388476c10fcb
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-04-24 14:44:41 -0300

    Começando trabalho

---
+++ Exercícios POO(Java)/trabalho/src/pacote_enum/Tipo_Salario.java
@@ -0,0 +1,5 @@
+package pacote_enum;
+
+public enum Tipo_Salario {
+
+}
---
+++ Exercícios POO(Java)/trabalho/src/teste/Teste_Funcionario.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class Teste_Funcionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
