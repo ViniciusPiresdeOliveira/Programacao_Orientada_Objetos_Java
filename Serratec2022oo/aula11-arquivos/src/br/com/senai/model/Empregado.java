bcbc282c2385ede678f62fb0ea3ad293f19bd807
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-04-26 21:26:18 -0300

    Novidade

---
+++ Serratec2022oo/aula11-arquivos/src/br/com/senai/model/Empregado.java
@@ -0,0 +1,41 @@
+package br.com.senai.model;
+
+import java.util.Objects;
+
+public class Empregado {
+
+	private String nome;
+	private String profissao;
+	public Empregado(String nome, String profissao) {
+		super();
+		this.nome = nome;
+		this.profissao = profissao;
+	}
+	@Override
+	public String toString() {
+		return "Empregado [nome=" + nome + ", profissao=" + profissao + "]";
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empregado other = (Empregado) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
+	
+}
---
+++ Serratec2022oo/aula11-arquivos/src/br/senai/arquivos/TesteLeituraGravacao.java
@@ -0,0 +1,50 @@
+package br.senai.arquivos;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import br.com.senai.model.Empregado;
+
+public class TesteLeituraGravacao {
+
+	public static void main(String[] args) {
+		Set<Empregado> empregados = new HashSet<>();
+
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Digite o nome do arquivo:");
+			String nomeArquivo = sc.next();
+			File arquivo = new File(nomeArquivo);
+			
+			Scanner sc1 = new Scanner(arquivo);
+			while (sc1.hasNextLine()) {
+				String linha = sc1.nextLine();
+				if(!linha.isEmpty()) {
+					String [] dadosLinha = linha.split(";");
+					String nome = dadosLinha[0];
+					String profissao = dadosLinha[1];
+					empregados.add(new Empregado(nome, profissao));
+				}
+			}
+			sc1.close();		
+			
+			FileWriter caminho = new FileWriter("/exemplos/saida.txt");
+			PrintWriter gravar = new PrintWriter(caminho);
+			for (Empregado empregado : empregados) {
+				String linhaArquivo = empregado.getNome() + ";" + empregado.getProfissao() + "\n";
+				gravar.printf(linhaArquivo);
+			}
+			gravar.close();
+			System.out.println("Arquivo gravado com sucesso !!!");
+		} catch (Exception e) {
+			System.out.println("Erro de leitura de gravação !!!");
+		}
+
+		
+	}
+
+}
