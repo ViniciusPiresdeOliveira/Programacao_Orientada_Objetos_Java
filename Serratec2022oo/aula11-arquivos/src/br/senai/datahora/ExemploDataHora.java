369857a974e5b5f7dc9e9d13594809a048e1eecf
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-04-27 10:01:14 -0300

    Atualizações

--- Serratec2022oo/aula11-arquivos/src/br/senai/datahora/ExemploDataHora.java
+++ Serratec2022oo/aula11-arquivos/src/br/senai/datahora/ExemploDataHora.java
@@ -8,26 +8,25 @@ import java.time.format.DateTimeFormatter;
 public class ExemploDataHora {
 
 	public static void main(String[] args) {
-		/*LocalTime hora = LocalTime.now();
-		System.out.println(hora);
+		LocalTime hora1 = LocalTime.now();
+		System.out.println(hora1);
 		System.out.println(LocalTime.of(22, 10));
-		
 		LocalDate dataAtual = LocalDate.now();
 		LocalDateTime dataHora = LocalDateTime.of(2022, 4, 1, 22, 10);
 		System.out.println(dataHora);
 		System.out.println(LocalDateTime.now());
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		System.out.println(dataAtual.format(dtf));*/
-		LocalTime hora = LocalTime.now();
-		System.out.println(hora);
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(dataAtual.format(dtf1) + "\n");
+		System.out.println("================================= \n");
+		LocalTime hora2 = LocalTime.now();
+		System.out.println(hora2);
 		System.out.println(LocalTime.of(22,10));
-		
-		LocalDate dataAtual = LocalDate.now();
-		LocalDateTime dataHora = LocalDateTime.of(2022, 4, 1, 22, 10);
+		LocalDate dataAtual2 = LocalDate.now();
+		LocalDateTime dataHora2 = LocalDateTime.of(2022, 4, 1, 22, 10);
 		System.out.println(dataHora);
 		System.out.println(LocalDateTime.now());
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm:ss");
-		System.out.println(dataHora.format(dtf));
+		DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("hh:mm:ss");
+		System.out.println(dataHora.format(dtf2));
 	}
 
 }
