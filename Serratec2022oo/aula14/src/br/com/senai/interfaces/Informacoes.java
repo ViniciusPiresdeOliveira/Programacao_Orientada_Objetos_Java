6eecf49e6fb95471fd68efeacc28cd7836a13de2
ViniciusPiresdeOliveira <piresvinicius35@gmail.com>
2022-04-29 20:23:38 -0300

    Novas classes

---
+++ Serratec2022oo/aula14/src/br/com/senai/interfaces/Informacoes.java
@@ -0,0 +1,6 @@
+package br.com.senai.interfaces;
+
+public interface Informacoes {
+
+	
+}
---
+++ Serratec2022oo/aula14/src/br/com/senai/model/Pedido.java
@@ -0,0 +1,8 @@
+package br.com.senai.model;
+
+public class Pedido {
+
+	public void finalizarPedido() {
+		System.out.println("Pedido entregue com sucesso !");
+	}
+}
